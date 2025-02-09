382ef0bf2adde71998fca7ee9db647402e1e4e60
Colin Paul Chaney <colin.chaney@xrcomm.com>
2025-02-08 23:51:00 -0600

    Added figure 2 and the slide associated with it.

--- figures/figure-2/fig_gen.py
+++ figures/figure-2/fig_gen.py
@@ -3,6 +3,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 
+plt.rcParams['text.usetex'] = True
+
+figure_width = 13.8
+figure_ratio = 2.19
+
 fsig = 35e6
 wsig = 2*np.pi*fsig
 fc = 500e6
@@ -15,7 +20,21 @@ sig = np.sin(wsig*t)
 sig_c = np.sin(wc*t)
 sig_total = sig * sig_c
 
+title_font = 30
+axis_font = 16
+num_font = 14
+
+t = t * 1e9
+
 fig, (ax1, ax2, ax3) = plt.subplots(3,1)
+title = 'Amplitude Modulation - Time Domain Example'
+plt.suptitle(title, fontsize = title_font)
+
+ax3.set_xlabel("Time (ns)", fontsize = axis_font)
+ax1.set_ylabel("Voltage (V)", fontsize = axis_font)
+ax2.set_ylabel("Voltage (V)", fontsize = axis_font)
+ax3.set_ylabel("Voltage (V)", fontsize = axis_font)
+
 ax1.plot(t, sig)
 ax2.plot(t, sig_c)
 ax3.plot(t, sig_total)
@@ -28,5 +47,7 @@ ax1.grid()
 ax2.grid()
 ax3.grid()
 
-plt.show()
+fig.set_size_inches(figure_width, figure_width/figure_ratio, forward=True)
+plt.tight_layout(pad=1.08, h_pad=None, w_pad=None, rect=None)
+fig.savefig("figure.png")
 
